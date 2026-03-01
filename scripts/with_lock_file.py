6eaf6c8827aa656627d6840624a55929084ac114
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2026-03-01 16:28:21 +0100

    set equal for `with_lock_file.py`

--- scripts/with_lock_file.py
+++ scripts/with_lock_file.py
@@ -1,8 +1,8 @@
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
-#   "requests>=2.32.2",
-#   "rich>=13.7.1",
+#   "requests==2.32.2",
+#   "rich==13.7.1",
 # ]
 # ///
 
--- scripts/without_lock_file.py
+++ scripts/without_lock_file.py
@@ -3,7 +3,7 @@
 # dependencies = [
 #   "httpx==0.27.1",
 #   "requests==2.32.2",
-#   "rich>=13.7.1",
+#   "rich==13.7.1",
 # ]
 # ///
 
